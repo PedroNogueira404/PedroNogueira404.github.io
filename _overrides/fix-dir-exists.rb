004f725a2a92f99f036358d3d5cba78b461b771c
Pedro Henrique Nogueira <nogueirapedro404@gmail.com>
2025-06-06 17:47:22 -0500

    fixed giscus changing of theme + playing dark css

--- _overrides/fix-dir-exists.rb
+++
@@ -1,7 +0,0 @@
-# Patch jekyll-multiple-languages-plugin to support Ruby 3.3
-module Dir
-  class << self
-    alias_method :exists?, :exist? unless respond_to?(:exists?)
-  end
-end
-puts "[✓] Patch Dir.exists? carregado com sucesso! "
